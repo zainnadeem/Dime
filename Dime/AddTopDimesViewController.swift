d042ec3473dddf4005b117363040b2a19004b056
Lloyd1nSpace <lloyd1nspace@icloud.com>
2017-03-13 10:58:48 -0400

    Working on Listing and adding Top Dimes

--- Dime/AddTopDimesViewController.swift
+++ Dime/AddTopDimesViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class AddTopDimeViewController: UIViewController {
     
+    let store = DataStore.sharedInstance
+    
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    @IBOutlet weak var addButton: UIBarButtonItem!
+   
+    @IBAction func addButtonTapped(_ sender: Any) {
+        
+        let destination = SearchViewControllerType.searchAllUsers as! SearchDimeViewController
+        
+        present(destination, animated: true, completion: nil)
+        
+        
+        print("Add button tapped!")
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
 extension AddTopDimeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        guard let topDimesCount = store.currentUser?.topFriends.count else {
+            return 1
+        }
+        return topDimesCount
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
